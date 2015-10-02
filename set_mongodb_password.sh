40b3015a7ad315231d83f3ae7b49dfb010deaa58
Alex Lizenberg <alexlz@web.de>
2015-10-02 12:08:36 +0200

    Update set_mongodb_password.sh

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -10,7 +10,7 @@ echo "=> Creating an admin user in MongoDB"
 mongo admin --eval "db.createUser({user: 'admin', pwd: '$PWD_ROOT', roles:['root']});"
 mongo admin -u admin -p $PWD_ROOT --eval "db.getSiblingDB('sdgtlctest').createUser({user: 'sdg', pwd: '$PWD_SDG', roles:['dbAdmin']});"
 mongo admin -u admin -p $PWD_ROOT --eval "db.getSiblingDB('sdg').createUser({user: 'sdg', pwd: '$PWD_SDG', roles:['dbAdmin']});"
-mongo admin -u admin -p $PDW_ROOT --eval "db.getSiblingDB('sdgtlc').createUser({user: 'sdg', pwd: '$PWD_SDG', roles:['dbAdmin']});"
+mongo admin -u admin -p $PWD_ROOT --eval "db.getSiblingDB('sdgtlc').createUser({user: 'sdg', pwd: '$PWD_SDG', roles:['dbAdmin']});"
 
 
 echo "=> Done!"
